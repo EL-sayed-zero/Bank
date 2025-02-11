6b118f1c78c9194441cbdd36b92884a85a8a1a08
A7med10Elbabry <ahmad1462004@gmail.com>
2025-02-11 20:51:13 +0200

    virtual remove all employee

--- bank_system/DataSourceInterface.h
+++ bank_system/DataSourceInterface.h
@@ -7,4 +7,5 @@ class DataSourceInterface
 	virtual void add_Employee(Employee person) = 0;
 	virtual void add_Client(Client person) = 0;
 	virtual vector<Employee> get_all_employee() = 0;
+	virtual void remove_all_employee() = 0;
 };
