20f73526a07eef686dea7aa18096d81c6aa7a64b
Elsayed Hesham <elsayedheshamdarwish@gmail.com>
2025-02-11 18:17:59 +0200

    add methods

--- bank_system/DataSourceInterface.h
+++ bank_system/DataSourceInterface.h
@@ -1,7 +1,9 @@
 #pragma once
 #include"Employee.h"
+#include"Client.h"
 class DataSourceInterface
 {
 	virtual void add_Employee(Employee person) = 0;
+	virtual void add_Client(Client person) = 0;
 
 };
