5ab0f33f4bb31c1101de98d2dbcfc81b49fe7ca2
Elsayed Hesham <elsayedheshamdarwish@gmail.com>
2025-02-13 06:06:24 +0200

    add operator

--- bank_system/Employee.h
+++ bank_system/Employee.h
@@ -5,6 +5,7 @@ using namespace std;
 #include<string>
 #include "Person.h"
 #include"Validation.h"
+#include<utility>
 
 class Employee :
     public Person
@@ -46,6 +47,29 @@ public:
         cout << salary << endl;
         cout << "##################\n";
     }
+
+
+
+
+
+
+   friend istream& operator >>(istream& in, Employee& employee) {
+       int temp_id;
+       double temp_balnce;
+       string temp_pw, temp_name;
+
+       cout << "enter id " << endl;
+       in >> temp_id;
+       cout << "enter name " << endl;
+       in >> temp_name;
+       cout << "enter balance " << endl;
+       in >> temp_balnce;
+       cout << "enter password " << endl;
+       in >> temp_pw;
+
+       Employee  move(Employee(temp_balnce, temp_id, temp_name, temp_pw));
+       return in;
+   }
 
 
 
